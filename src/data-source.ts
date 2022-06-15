41f0d619f8cf3c54b32980ea8366426edcf00518
heitorl <juniorio01@hotmail.com>
2022-06-14 21:56:54 -0300

    deploy configuretion

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,8 +8,13 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
-  //   synchronize: true,
-  logging: false,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+
+  synchronize: false,
+  logging: true,
   entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
   migrations: [path.join(__dirname, "/migrations/**/*.{ts,js}")],
 });
