41356709f8cd8f60a61a8c55495d4b4fd0593784
heitorl <juniorio01@hotmail.com>
2022-06-14 22:20:57 -0300

    deploy 6

--- src/services/collaborator.service.ts
+++ src/services/collaborator.service.ts
@@ -1,4 +1,4 @@
-import { hash } from "bcrypt";
+import { hash } from "bcryptjs";
 import { Request } from "express";
 import { sign } from "jsonwebtoken";
 import { Collaborators } from "../entities";
